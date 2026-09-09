be968d63786bf7b2054a4bf801b00de08ba11e4c
pp80768241-alt <pp80768241@gmail.com>
2026-09-10 00:10:09 +0530

    Initial JARVIS assistant

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "ambarsariya ": "https://www.youtube.com/watch?v=oMesPehN_Do&list=RDoMesPehN_Do&start_radio=1&pp=ygUQYW1iYXJzYXJpeWEgc29uZ6AHAQ%3D%3D",
+    "GOAT": "https://www.youtube.com/watch?v=M8vDwlHigJA&list=RDM8vDwlHigJA&start_radio=1&pp=ygUJZ29hdCBzb25noAcB0gcJCf4LAYcqIYzv"
+
+
+}
